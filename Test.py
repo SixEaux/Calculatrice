b2bd580eb638bcea8f69fd25e8990b9033999fd1
mrbah <rockleebaha@gmail.com>
2024-10-24 11:23:11 +0200

    La Calculadora

--- Test.py
+++ Test.py
@@ -1,2 +1,18 @@
-print("Hello world")
-print("Fuck the police")
+def somme(x,y):
+    return(x+y)
+def substract(x,y):
+    return(x-y)
+def multip(x,y):
+    return(x*y)
+def divis(x,y):
+    if y==0:
+        return('error')
+    else:
+        return(x/y)
+def powerof(x,y):
+    return(x**y)
+def roots(x,y):
+    if y==0:
+        return('error')
+    else:
+        return(x**(1/y))
