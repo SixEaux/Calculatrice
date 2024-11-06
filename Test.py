f4110160de0ff36566a81898c4a654975932dc86
SixEaux <dc200421@gmail.com>
2024-11-06 16:13:57 +0100

    Test

--- Test.py
+++ Test.py
@@ -17,13 +17,6 @@ class Calculatrice:
     def boutons(self):
         Entry(self.root, width=100, bg='orange', textvariable=self.res).place(x=50, y=10)
 
-        # Button(self.root, text="+", command=lambda:self.affiche('+')).place(x=50,y=50)
-        # Button(self.root, text="-", command=lambda: self.affiche('-')).place(x=50,y=100)
-        # Button(self.root, text="x", command=lambda: self.affiche('x')).place(x=50,y=150)
-        # Button(self.root, text="/", command=lambda: self.affiche('/')).place(x=50,y=200)
-        # Button(self.root, text="^", command=lambda: self.affiche('^')).place(x=50,y=250)
-        # Button(self.root, text="Root", command=lambda: self.affiche('Root')).place(x=50,y=300)
-
 
         tab = ['+','-', 'x', '/', '^', '(', ')']
         c=50
@@ -31,14 +24,13 @@ class Calculatrice:
             Button(self.root, text=i, command=lambda a=i: self.affiche(a)).place(x=50, y=c)
             c+=50
 
-        Button(self.root, text="+/-", command=lambda: self.changesigne()).place(x=50, y=500)
 
 
         for i in range(10):
             Button(self.root, text=str(i), command=lambda a=i: self.affiche(str(a))).place(x=300, y=i * 50 + 50)
 
         Button(self.root, text='=', command=lambda: self.resoudre()).place(x=500, y=50)
-        Button(self.root, text='C', command=lambda: self.AC()).place(x=500, y=100)
+        Button(self.root, text='C', command=lambda: self.AC()).place(x=500, y=150)
         Button(self.root, text='Del', command=lambda: self.retour()).place(x=500, y=100)
 
 
@@ -63,37 +55,6 @@ class Calculatrice:
         self.valeur += str(valeur)
         self.res.set(self.valeur)
 
-
-
-
-
-
-
-
-
-    def somme(self, y):
-            return (x + y)
-
-    def substract(self, y):
-            return (x - y)
-
-    def multip(self, y):
-            return (x * y)
-
-    def divis(self, y):
-            if y == 0:
-                return ('error')
-            else:
-                return (x / y)
-
-    def powerof(self, y):
-            return (x ** y)
-
-    def roots(self, y):
-            if y == 0:
-                return ('error')
-            else:
-                return (x ** (1 / y))
 
 
     def calculate_expression(self):
