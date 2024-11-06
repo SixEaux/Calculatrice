ea2f26138dc6068733b5f2a3b39b679fe79f1bab
SixEaux <dc200421@gmail.com>
2024-11-06 16:35:37 +0100

    Test

--- Test.py
+++ Test.py
@@ -1,13 +1,13 @@
 from tkinter import *
 from tkinter import ttk
-
+import random
 
 class Calculatrice:
-    def __init__(self,root):
+    def __init__(self,root, p=False):
         self.root = root
         self.root.geometry('600x600')
         self.root.title('Calculatrice')
-
+        self.p=p
         self.res = StringVar()
         self.valeur = ''
         self.boutons()
@@ -15,23 +15,31 @@ class Calculatrice:
 
 
     def boutons(self):
-        Entry(self.root, width=100, bg='orange', textvariable=self.res).place(x=50, y=10)
+        Entry(self.root, width=100, bg='orange', textvariable=self.res).place(x=0, y=20)
 
 
         tab = ['+','-', 'x', '/', '^', '(', ')']
         c=50
         for i in tab:
-            Button(self.root, text=i, command=lambda a=i: self.affiche(a)).place(x=50, y=c)
+            Button(self.root, text=i, command=lambda a=i: self.affiche(a), width= 10, height=3).place(x=50, y=c)
             c+=50
 
 
+        if not self.p:
+            for i in range(10):
+                Button(self.root, text=str(i), command=lambda a=i: self.affiche(str(a))).place(x=300, y=i * 50 + 50)
 
-        for i in range(10):
-            Button(self.root, text=str(i), command=lambda a=i: self.affiche(str(a))).place(x=300, y=i * 50 + 50)
+        elif self.p:
+            t=[str(i) for i in range(10)]
+            for i in range(10):
+                a=random.choice(t)
+                Button(self.root, text=a, command=lambda h=a: self.affiche(h)).place(x=300, y=i * 50 + 50)
+                t.remove(a)
 
-        Button(self.root, text='=', command=lambda: self.calculate_expression()).place(x=500, y=50)
-        Button(self.root, text='C', command=lambda: self.AC()).place(x=500, y=150)
-        Button(self.root, text='Del', command=lambda: self.retour()).place(x=500, y=100)
+        Button(self.root, text='=', command=lambda: self.calculate_expression(), width= 10, height=3).place(x=500, y=50)
+        Button(self.root, text='C', command=lambda: self.AC(), width= 10,height=3).place(x=500, y=150)
+        Button(self.root, text='Del', command=lambda: self.retour(), width= 10, height=3).place(x=500, y=100)
+
 
 
 
@@ -69,9 +77,7 @@ class Calculatrice:
 
 
 root=Tk()
-calculette = Calculatrice(root)
-
-
+calculette = Calculatrice(root,True)
 
 
 root.mainloop()
