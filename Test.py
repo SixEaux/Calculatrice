68669b7a00182f4076355abd6f6fae7253b95256
SixEaux <dc200421@gmail.com>
2024-11-06 16:02:25 +0100

    Test

--- Test.py
+++ Test.py
@@ -17,26 +17,23 @@ class Calculatrice:
     def boutons(self):
         Entry(self.root, width=100, bg='orange', textvariable=self.res).place(x=50, y=10)
 
-        # Button(self.root, text="+", command=lambda:self.affiche('+')).place(x=50,y=50)
-        # Button(self.root, text="-", command=lambda: self.affiche('-')).place(x=50,y=100)
-        # Button(self.root, text="x", command=lambda: self.affiche('x')).place(x=50,y=150)
-        # Button(self.root, text="/", command=lambda: self.affiche('/')).place(x=50,y=200)
-        # Button(self.root, text="^", command=lambda: self.affiche('^')).place(x=50,y=250)
-        # Button(self.root, text="Root", command=lambda: self.affiche('Root')).place(x=50,y=300)
 
-
-
-        tab = ['+','-', 'x', '/', '^', 'Root', '=']
+        tab = ['+','-', 'x', '/', '^', '(', ')']
         c=50
         for i in tab:
             Button(self.root, text=i, command=lambda a=i: self.affiche(a)).place(x=50, y=c)
             c+=50
 
-        Button(self.root, text="+/-", command=lambda: self.changesigne()).place(x=50, y=500)
-
-
         for i in range(10):
             Button(self.root, text=str(i), command=lambda a=i: self.affiche(str(a))).place(x=300, y=i * 50 + 50)
+
+        Button(self.root, text='=', command=lambda: self.resoudre()).place(x=500, y=50)
+        Button(self.root, text='C', command=lambda: self.AC()).place(x=500, y=100)
+        Button(self.root, text='Del', command=lambda: self.retour()).place(x=500, y=100)
+
+
+    def resoudre(self):
+        pass
 
 
     def AC(self):
@@ -48,103 +45,13 @@ class Calculatrice:
         self.res.set(self.valeur)
 
     def changesigne(self):
-        self.valeur = '-' + self.valeur
+        self.valeur = '-' + '(' + self.valeur + ')'
         self.res.set(self.valeur)
 
     def affiche(self,valeur):
         self.valeur += str(valeur)
         self.res.set(self.valeur)
 
-
-
-
-
-
-
-
-
-    def somme(self, y):
-            return (x + y)
-
-    def substract(self, y):
-            return (x - y)
-
-    def multip(self, y):
-            return (x * y)
-
-    def divis(self, y):
-            if y == 0:
-                return ('error')
-            else:
-                return (x / y)
-
-    def powerof(self, y):
-            return (x ** y)
-
-    def roots(self, y):
-            if y == 0:
-                return ('error')
-            else:
-                return (x ** (1 / y))
-
-
-
-
-
-
-
-
-
-
-
-
-
-# bouttext=Button(root, text="Afficher", command=choinomb)
-# bouttext.pack(side=TOP, padx=50, pady=10)
-#
-# entry = Entry(root)
-# entry.pack()
-#
-# frame = Frame(root)
-# frame.pack()
-# root.geometry("500x200")
-#
-# label = ttk.Label(frame, text=f"{int(x)}" )
-# label.pack(side="left")
-# label2 = ttk.Label(frame, text=f"{int(y)}")
-# label2.pack(side="bottom")
-#
-# def on_button_click(operation):
-#     if operation == "add":
-#         result = somme(x, y)
-#         res = Label(root, text=f"{int(result)}")
-#         res.pack(side="bottom")
-#         print(result)
-#     elif operation == "sub":
-#         result = substract(x, y)
-#         res = Label(root, text=f"{int(result)}")
-#         res.pack(side="right")
-#         print(result)
-#     elif operation == "mul":
-#         result = multip(x, y)
-#         res = Label(root, text=f"{int(result)}")
-#         res.pack(side="bottom")
-#         print(result)
-#     elif operation == "div":
-#         result = divis(x, y)
-#         res = Label(root, text=f"{int(result)}")
-#         res.pack(side="bottom")
-#         print(result)
-#     elif operation == "pow":
-#         result = powerof(x, y)
-#         res = Label(root, text=f"{int(result)}")
-#         res.pack(side="bottom")
-#         print(result)
-#     elif operation == "root":
-#         result = roots(x, y)
-#         res = Label(root, text=f"{int(result)}")
-#         res.pack(side="bottom")
-#         print(result)
 
 
 
